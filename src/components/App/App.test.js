294ec6c1b68f1b4a2d650ec4733ac9d824135788
Edwin Rivera <waehake@gmail.com>
2019-05-07 11:10:33 -0400

    added a a describe block for the purposes of behavior driven development, since we are simulating click events when adding a new object to an array and rendering a new gift to the DOM.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -9,29 +9,31 @@ import App from "./App";
 // required to make enzyme work
 configure({ adapter: new Adapter() });
 
-const app = shallow(<App />);
+describe("App", () => {
+  const app = shallow(<App />);
 
-it("expects app to work", () => {
-  expect(app).toMatchSnapshot();
-});
+  it("expects app to work", () => {
+    expect(app).toMatchSnapshot();
+  });
 
-it("initializes the `state` to contain an empty array of gifts", () => {
-  // checks the state object inside of the app component for an object called gift. The gift object should contain an empty array.
-  expect(app.state().gifts).toEqual([]);
-});
+  it("initializes the `state` to contain an empty array of gifts", () => {
+    // checks the state object inside of the app component for an object called gift. The gift object should contain an empty array.
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it("adds a new gift to the `state` by clicking the add gift button", () => {
-  // use the find method to look for the react component that contains the class .btn-add
-  // use the simulate function to mock user interaction
-  app.find(".btn-add").simulate("click");
-  //
-  expect(app.state().gifts).toEqual([{ id: 1 }]);
-});
+  it("adds a new gift to the `state` by clicking the add gift button", () => {
+    // use the find method to look for the react component that contains the class .btn-add
+    // use the simulate function to mock user interaction
+    app.find(".btn-add").simulate("click");
+    //
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+  });
 
-it("adds a new gift to the rendered list when clicking the add gift button", () => {
-  // use the find method
-  // use the simulate function to mock user interaction
-  app.find(".btn-add").simulate("click");
-  // find the dom node with gift-list then look at all of its children nodes and check that the length is equal 1.
-  expect(app.find(".gift-list").children().length).toEqual(2);
+  it("adds a new gift to the rendered list when clicking the add gift button", () => {
+    // use the find method
+    // use the simulate function to mock user interaction
+    app.find(".btn-add").simulate("click");
+    // find the dom node with gift-list then look at all of its children nodes and check that the length is equal 1.
+    expect(app.find(".gift-list").children().length).toEqual(2);
+  });
 });
