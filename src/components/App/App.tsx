3f36daa9df4ebcb94238f12afaf62cc4cbf3ec50
Edwin Rivera <waehake@gmail.com>
2019-05-07 10:47:15 -0400

    added a test for creating a new object to the gifts state when a user clicks on the add gift button.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,37 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 interface Gift {
   id: number;
 }
 
 interface State {
-  gifts: Array<Gift>;
+  gifts: Array<Gift | any>;
 }
 
 class App extends React.Component<{}, State> {
   state = {
     gifts: []
   };
+
+  addGift = () => {
+    const { gifts } = this.state;
+    // get a list of all of the ids inside the gifts array
+    let ids = gifts.map(gift => gift.id);
+    // get the maximum id inside of the ids array account for the possiblity of there being no items
+    let maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+    this.setState({
+      gifts: [...gifts, { id: maxId + 1 }]
+    });
+  };
   render() {
     return (
       <div>
         <h1>App Test SnapShot</h1>
-        <button className="btn-add">Add</button>
+        <Button className="btn-add" onClick={this.addGift}>
+          Add
+        </Button>
       </div>
     );
   }
