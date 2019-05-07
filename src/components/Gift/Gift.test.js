98ca21d17832bb076265c499e165fbf349cd3319
Edwin Rivera <waehake@gmail.com>
2019-05-07 11:22:17 -0400

    added a new test suite called Gift to test the Gift component.

---
+++ src/components/Gift/Gift.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+// Shallow renders the root node and returns a shallow wrapper around it. It must be a single-node wrapper.
+//
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import Gift from "./Gift";
+
+// required to make enzyme work
+configure({ adapter: new Adapter() });
+
+describe("Gift", () => {
+  const gift = shallow(<Gift />);
+
+  it("expects Gift to work", () => {
+    expect(gift).toMatchSnapshot();
+  });
+});
---
+++ src/components/Gift/Gift.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Gift extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Gift</h1>
+      </div>
+    );
+  }
+}
+
+export default Gift;
