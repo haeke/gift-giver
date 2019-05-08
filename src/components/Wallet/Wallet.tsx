29f9da00120dd9ea5c9705b36c40e9e43ee90de0
haeke <riverae2@student.wpunj.edu>
2019-05-08 15:21:08 -0400

    Added a test to the Wallet component to confirm that the input changes when adding a new value.

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -3,14 +3,24 @@ import { connect } from "react-redux";
 export class Wallet extends Component<{ balance: number }> {
   constructor(props) {
     super(props);
+
+    this.state = {
+      balance: undefined
+    };
   }
+
+  updateBalance = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      balance: parseInt(event.target.value, 10)
+    });
+  };
 
   render() {
     return (
       <div>
         <h1 className="balance">Wallet balance: {this.props.balance}</h1>
         <br />
-        <input className="input-wallet" />
+        <input className="input-wallet" onChange={this.updateBalance} />
       </div>
     );
   }
