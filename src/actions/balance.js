b891cb166c96a7bc2cd35db1cf1c61d4dd48ba20
haeke <riverae2@student.wpunj.edu>
2019-05-08 14:35:20 -0400

    added a depositAction action creator, with a test to create the deposit action inside balance.test.js

--- src/actions/balance.js
+++ src/actions/balance.js
@@ -1,8 +1,15 @@
-import { SET_BALANCE } from "./constants";
+import { SET_BALANCE, DEPOSIT } from "./constants";
 
 export const setBalance = balance => {
   return {
     type: SET_BALANCE,
     balance
+  };
+};
+
+export const depositAction = amount => {
+  return {
+    type: DEPOSIT,
+    deposit: amount
   };
 };
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,10 +1,19 @@
 import balanceReducer from "./balance";
-import { SET_BALANCE } from "../actions/constants";
+import { SET_BALANCE, DEPOSIT } from "../actions/constants";
+import { depositAction } from "../actions/balance";
 
 describe("balanceReducer", () => {
   it("sets a balance", () => {
     const balance = 10;
 
     expect(balanceReducer(0, { type: SET_BALANCE, balance })).toEqual(balance);
+  });
+
+  it("creates an action to deposit into the balance", () => {
+    const deposit = 20;
+
+    const expectedAction = { type: DEPOSIT, deposit };
+
+    expect(depositAction(deposit)).toEqual(expectedAction);
   });
 });
