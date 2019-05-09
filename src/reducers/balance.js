fdc826c3f40d9c805c093ce523da42984e04b55e
Edwin Rivera <waehake@gmail.com>
2019-05-09 08:52:54 -0400

    the reinitialize test is now working, the balance reducer now uses cookies to persist the balance variable when the browser is refreshed.

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -1,14 +1,26 @@
 import { SET_BALANCE, DEPOSIT, WITHDRAWAL } from "../actions/constants";
+import { read_cookie, bake_cookie } from "sfcookies";
+
+const BALANCE_COOKIE = "BALANCE_COOKIE";
 
 export default (state = 0, action) => {
+  let balance;
   switch (action.type) {
     case SET_BALANCE:
-      return action.balance;
+      balance = action.balance;
+      break;
     case DEPOSIT:
-      return state + action.deposit;
+      balance = state + action.deposit;
+      break;
     case WITHDRAWAL:
-      return state - action.withdrawal;
+      balance = state - action.withdrawal;
+      break;
     default:
-      return state;
+      balance = parseInt(read_cookie(BALANCE_COOKIE), 10) || state;
   }
+
+  // save the new balance to the cookie
+  bake_cookie(BALANCE_COOKIE, balance);
+
+  return balance;
 };
