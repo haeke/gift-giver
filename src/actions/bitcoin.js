51da5e5e86ca87344b269a6e5491b3020a489c7f
Edwin Rivera <waehake@gmail.com>
2019-05-09 11:44:04 -0400

    changed the fetchBitcoin method such that it doesnt use async await to get the data stream.

--- src/actions/bitcoin.js
+++ src/actions/bitcoin.js
@@ -1,16 +1,29 @@
 import { FETCH_BITCOIN } from "./constants";
 // fetchBitcoin uses redux thunk and async await to request data from the coindesk api. Need to add proper error handling.
-export const fetchBitcoin = () => async dispatch => {
-  try {
-    let response = await fetch(
-      "https://api.coindesk.com/v1/bpi/currentprice.json"
-    );
+// export const fetchBitcoin = () => async dispatch => {
+//   try {
+//     let response = await fetch(
+//       "https://api.coindesk.com/v1/bpi/currentprice.json"
+//     );
 
-    dispatch({
-      type: FETCH_BITCOIN,
-      bitcoin: response.body
-    });
-  } catch (error) {
-    console.error(error);
-  }
+//     dispatch({
+//       type: FETCH_BITCOIN,
+//       bitcoin: response.body
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+export const fetchBitcoin = () => {
+  return dispatch => {
+    return fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => response.json())
+      .then(json =>
+        dispatch({
+          type: FETCH_BITCOIN,
+          bitcoin: json
+        })
+      );
+  };
 };
