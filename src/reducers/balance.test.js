a2fe96a4a08d0f7dce2543affe64243882a3cdd8
Edwin Rivera <waehake@gmail.com>
2019-05-09 08:45:26 -0400

    added a test for re-initializing the balance when a user refreshes their browser.

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,11 +1,21 @@
 import balanceReducer from "./balance";
+import balanceReducer2 from "./balance";
 import { SET_BALANCE, DEPOSIT, WITHDRAWAL } from "../actions/constants";
 
 describe("balanceReducer", () => {
-  it("sets a balance", () => {
+  describe("when the reducer is initialized", () => {
     const balance = 10;
+    it("sets a balance", () => {
+      expect(balanceReducer(0, { type: SET_BALANCE, balance })).toEqual(
+        balance
+      );
+    });
 
-    expect(balanceReducer(0, { type: SET_BALANCE, balance })).toEqual(balance);
+    describe("re-initializing balance with balanceReducer2", () => {
+      it("reads the balance from cookies", () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
 
   it("creates an action to deposit into the balance", () => {
