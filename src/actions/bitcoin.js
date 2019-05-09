55d42d1921f1c5ae8446b25cf49e1ee54a1348db
Edwin Rivera <waehake@gmail.com>
2019-05-09 10:02:16 -0400

    added a try catch block to the api request, I should make an error reducer for better error handling.

--- src/actions/bitcoin.js
+++ src/actions/bitcoin.js
@@ -1,12 +1,16 @@
 import { FETCH_BITCOIN } from "./constants";
-
+// fetchBitcoin uses redux thunk and async await to request data from the coindesk api. Need to add proper error handling.
 export const fetchBitcoin = () => async dispatch => {
-  let response = await fetch(
-    "https://api.coindesk.com/v1/bpi/currentprice.json"
-  );
+  try {
+    let response = await fetch(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
 
-  dispatch({
-    type: FETCH_BITCOIN,
-    bitcoin: response.body
-  });
+    dispatch({
+      type: FETCH_BITCOIN,
+      bitcoin: response.body
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
