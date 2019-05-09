2effd5e96a001f09edd12a094832881bad544488
Edwin Rivera <waehake@gmail.com>
2019-05-09 10:13:09 -0400

    added the bitcoin reducer to the combineReducers function.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import balanceReducer from "./balance";
+import bitcoinReducer from "./bitcoin";
 
 // The root reducer object will make adding new reducers maintainable
 export default combineReducers({
-  balance: balanceReducer
+  balance: balanceReducer,
+  bitcoin: bitcoinReducer
 });
