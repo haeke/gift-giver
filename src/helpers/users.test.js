5df3d1c6b860461b6b8e39d06886824b733883a7
haeke <riverae2@student.wpunj.edu>
2019-05-15 15:32:49 -0400

    added a unit test for the changeEmail function.

--- src/helpers/users.test.js
+++ src/helpers/users.test.js
@@ -2,13 +2,18 @@ import { User } from "./users";
 
 describe("the User class instance", () => {
   let edwin = new User("Edwin", "erivera@gmail.com", [], 0);
+  edwin.saveScore(80);
+  edwin.saveScore(78);
   it("Creates a user with name Edwin", () => {
     expect(edwin.name).toEqual("Edwin");
   });
 
   it("Adds test scores for the user", () => {
-    edwin.saveScore(80);
-    edwin.saveScore(78);
     expect(edwin.showNameAndScores()).toEqual("Edwin scores: 80,78");
+  });
+
+  it("allows a user to change their email", () => {
+    edwin.changeEmail("e@gmail.com");
+    expect(edwin.email).toEqual("e@gmail.com");
   });
 });
